c3748316aa1cb918b351844b22b0b3dd6346ec6e
WMSShehara <swadumes20@students.oamk.com>
2025-05-09 15:19:11 +0300

    changed title

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   return (
     <div className="container">
       <header className="header">
-        <h1>3GPP Flow Editor</h1>
+        <h1>3GPP Procedure insights</h1>
       </header>
 
       <div className="grid-layout">
